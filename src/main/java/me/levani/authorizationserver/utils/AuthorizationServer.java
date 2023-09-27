66727599104783e8ec80d588e6fe6594289da771
Levani <levaniiantbelidze@gmail.com>
2023-09-27 09:15:40 +0400

    make it library

---
+++ src/main/java/me/levani/authorizationserver/utils/AuthorizationServer.java
@@ -0,0 +1,9 @@
+package me.levani.authorizationserver.utils;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan("me.levani.authorizationserver")
+@SpringBootApplication
+public @interface AuthorizationServer {
+}
